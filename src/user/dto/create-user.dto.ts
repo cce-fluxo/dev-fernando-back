a4ae3e81d0e4dc376d51f811a6ac369283353711
Kauã Gomes <70298794+Kauagpaiva@users.noreply.github.com>
2025-02-03 00:52:28 -0300

    implement create user

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { User } from '../entities/user.entity';
+import {
+  IsEmail,
+  IsString,
+} from 'class-validator';
+
+export class CreateUserDto extends User {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  name: string;
+}
